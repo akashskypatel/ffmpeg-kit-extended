129427fa20a241ead7114cfaea2de6c8a08badbe
akashskypatel <akashskypatel@gmail.com>
2026-09-17 17:41:51 -0700

    revert: remove unsupported flutter wasm test workaround

--- flutter/lib/src/platform/web/wasm_loader.dart
+++ flutter/lib/src/platform/web/wasm_loader.dart
@@ -11,10 +11,7 @@ final class WasmLoader {
   WasmLoader._();
 
   static final instance = WasmLoader._();
-  static const assetRoot = String.fromEnvironment(
-    'FFMPEG_KIT_EXTENDED_WEB_ASSET_ROOT',
-    defaultValue: 'assets/packages/ffmpeg_kit_extended_flutter/wasm',
-  );
+  static const assetRoot = 'assets/packages/ffmpeg_kit_extended_flutter/wasm';
 
   JSObject? _module;
   Future<void>? _initializing;
